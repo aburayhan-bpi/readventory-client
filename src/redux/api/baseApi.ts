388f4c6166dda937f381c1a6ddfe4ad38748f452
Abu Rayhan <aburayhan.bpi@gmail.com>
2025-07-05 22:34:02 +0600

    fix api issues

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:4000/api",
+    baseUrl: "https://readventory-server.vercel.app/api",
   }),
   tagTypes: ["book", "borrow"],
   endpoints: (builder) => ({
